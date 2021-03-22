1c8b74643de116d28fb4ae955696b54c47486a8d
Antoine07 <antoine.lucsko@gmail.com>
2021-03-22 12:20:52 +0100

    parser service

---
+++ bar/src/Services/HelperParser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use cebe\markdown\Markdown;
+
+class HelperParser{
+
+    private $parser;
+
+    public function __construct(Markdown $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    public function translateHtml(array $markdowns):array{
+
+        $translate = [];
+        foreach($markdowns as $markdown){
+            $translate[] = $this->parser->parse($markdown);
+        }
+
+        return $translate;
+    }
+}
